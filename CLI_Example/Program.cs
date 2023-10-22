0fa6225ec9f6a4b323d39efc9d0e68dd4e302f11
WCramRTC <91401050+WCramRTC@users.noreply.github.com>
2023-10-21 21:20:25 -0700

    Initial Commit

---
+++ CLI_Example/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace CLI_Example
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
